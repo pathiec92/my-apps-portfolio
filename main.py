33ce820f6321057fce48befc01068b03c54d95d1
Pampapathi K <pampapathi.k@zebra.com>
2024-03-17 16:53:29 +0530

    Added title, description, image and url for source code

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import streamlit as st
-
+import pandas
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image('images/profile.jpg', width=600)
+    st.image('images/profile.jpg')
 
 with col2:
     st.title("Pampapathi K")
@@ -14,3 +14,22 @@ with col2:
     I have inbuilt ability to acquire ever changing new technologies in this Era."""
     #st.write(content)
     st.info(content)
+    content2 = """Below is my contact details. Contact me if you have any requirement to develop any apps right from 
+    Android mobile to web app applications"""
+st.write(content2)
+
+col3, col4 = st.columns(2)
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+        st.image(f"images/{row['image']}")
+        st.info(row['description'])
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
+        st.image(f"images/{row['image']}")
+        st.info(row['description'])
+        st.write(f"[Source Code]({row['url']})")
